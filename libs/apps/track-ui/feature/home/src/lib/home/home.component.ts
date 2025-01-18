6e2222516d8317bc60648dbb2dc24a30265e3d23
Ruslan Gillyazanov <rgillyazanov@yandex.ru>
2025-01-18 22:11:45 +0500

    Добавил главную страницу

--- libs/apps/track-ui/feature/home/src/lib/home/home.component.ts
+++ libs/apps/track-ui/feature/home/src/lib/home/home.component.ts
@@ -1,23 +1,62 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnInit,
-} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AuthApiService } from '@tracker/core/services';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { ConfirmationService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Avatar } from 'primeng/avatar';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { InputText } from 'primeng/inputtext';
+import { Button } from 'primeng/button';
+import { FloatLabel } from 'primeng/floatlabel';
+import { Select } from 'primeng/select';
+import { OverlayBadge } from 'primeng/overlaybadge';
 
 @Component({
   selector: 'lib-home',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLinkActive,
+    RouterLink,
+    NgOptimizedImage,
+    Avatar,
+    IconField,
+    InputIcon,
+    InputText,
+    Button,
+    FloatLabel,
+    Select,
+    OverlayBadge,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  providers: [ConfirmationService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit {
-  private readonly _authApiService = inject(AuthApiService);
+export class HomeComponent {
+  navsTop = signal([
+    { label: 'Главная', icon: 'pi pi-home', path: '/home' },
+    { label: 'Команда', icon: 'pi pi-users', path: '/team' },
+  ]);
 
-  ngOnInit(): void {
-    this._authApiService.getUser().subscribe(console.log);
-  }
+  navsBottom = signal([
+    {
+      label: 'Настройки',
+      icon: 'pi pi-cog',
+      path: '/settings',
+    },
+  ]);
+
+  projects = signal([
+    {
+      name: 'IT-Grad.ru',
+    },
+    {
+      name: 'PetStory',
+    },
+    {
+      name: 'Роснефть',
+    },
+  ]);
 }
